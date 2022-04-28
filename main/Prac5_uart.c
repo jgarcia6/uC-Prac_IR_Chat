de290411ac68fdc6d84b7c4481729f5a7f90f39b
JG <garcia.jesus@uabc.edu.mx>
2022-04-28 00:08:06 -0700

    Working IR TX UART scheme

--- main/Prac5_uart.c
+++ main/Prac5_uart.c
@@ -11,31 +11,52 @@
 #include "sdkconfig.h"
 #include "myUart.h"
 
-#define UART_NUM        (0)
-#define UART_RX_PIN     (3)
-#define UART_TX_PIN     (1)
 
-#define UART_IR_DIV     6
+#define UART_SEND_SCHEME    0
+#define TIMER_SEND_SCHEME   1
 
-#define UART2_RX        16
-#define UART2_TX        17
-#define UART2_BAUDS     (700)
+// UART 0 used for PC communication
+#define PC_UART_PORT        0
+#define PC_UART_RX_PIN      3
+#define PC_UART_TX_PIN      1
+#define PC_UARTS_BAUD_RATE  (115200)
 
-#define UART1_TX        15
-#define UART1_RX        4
-#define UART1_BAUDS     (38000*3)
+// UART 2 used for IR RX
+#define IR_RX_UART_PORT     2
+#define IR_RX_RX_PIN        16
+#define IR_RX_TX_PIN        17
 
-#define UART_IR_TX      1
-#define UART_IR_RX      2
+#define UART_BUF_SIZE       (1024)
 
-#define UARTS_BAUD_RATE         (115200)
-#define TASK_STACK_SIZE         (1048)
-#define READ_BUF_SIZE           (1024)
+#define IR_FREQ             38000
+#define MAX_PACKET_SIZE     255
 
-#define IR_LOW     0x5B    //0b101 1011
-#define IR_HIGH    0x00
+#define SCHEME UART_SEND_SCHEME
+#if SCHEME == UART_SEND_SCHEME
+// UART 1 used for IR TX
+#define IR_TX_UART_PORT     1
+#define IR_TX_TX_PIN        15
+#define IR_TX_RX_PIN        4
 
-#define MAX_SIZE    255
+#define IR_BIT_SIZE         7
+#define IR_MUL              3
+#define IR_LOW              0x5B   //0b101 1011
+#define IR_HIGH             0x00
+#define UART_IR_DIV         6
+#define IR_TX_BAUDS         (IR_FREQ*IR_MUL)
+#define IR_RX_BAUDS         ((IR_FREQ / (IR_BIT_SIZE + 2)) / UART_IR_DIV) //= ~700
+#else
+#define IR_BIT_SIZE     8
+#define IR_LOW          0xF0//0x38   //0b 111000
+#define IR_MUL          9
+#define IR_TX_BAUDS     380000UL //(38000*IR_MUL)
+
+char caIrHigh[] = {IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,
+                    IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,IR_HIGH,};
+char caIrLow[] = {IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,
+                    IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW,IR_LOW};
+
+#endif
 
 /* Message printed by the "consoletest" command.
  * It will also be used by the default UART to check the reply of the second
@@ -60,7 +81,7 @@ void uartInit(uart_port_t uart_num, uint32_t baudrate, uint8_t size, uint8_t par
         .source_clk = UART_SCLK_APB,
     };
 
-    ESP_ERROR_CHECK(uart_driver_install(uart_num, READ_BUF_SIZE, READ_BUF_SIZE, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, UART_BUF_SIZE, UART_BUF_SIZE, 0, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(uart_num, txPin, rxPin,
                                  UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
@@ -107,7 +128,7 @@ char uartGetchar(uart_port_t uart_num)
         delayMs(10);
     }
     // read byte, no wait
-    uart_read_bytes(UART_NUM, &c, sizeof(c), 0);
+    uart_read_bytes(uart_num, &c, sizeof(c), 0);
 
     return c;
 }
@@ -116,21 +137,29 @@ void IR_SendBit(uint8_t bit)
 {
     if (bit)
     {   
+#if SCHEME == UART_SEND_SCHEME
         for (uint8_t i = 0; i < UART_IR_DIV; i++)
         {
-            uartPutchar(UART_IR_TX, IR_HIGH);
-            uartPutchar(UART_IR_TX, IR_HIGH);
-            uartPutchar(UART_IR_TX, IR_HIGH);
+            uartPutchar(IR_TX_UART_PORT, IR_HIGH);
+            uartPutchar(IR_TX_UART_PORT, IR_HIGH);
+            uartPutchar(IR_TX_UART_PORT, IR_HIGH);
         }
+#else
+    uart_write_bytes(IR_TX_UART_PORT, caIrHigh, sizeof(caIrHigh));
+#endif
     }
     else
     {        
+#if SCHEME == UART_SEND_SCHEME
         for (uint8_t i = 0; i < UART_IR_DIV; i++)
         {
-            uartPutchar(UART_IR_TX, IR_LOW);
-            uartPutchar(UART_IR_TX, IR_LOW);
-            uartPutchar(UART_IR_TX, IR_LOW);
+            uartPutchar(IR_TX_UART_PORT, IR_LOW);
+            uartPutchar(IR_TX_UART_PORT, IR_LOW);
+            uartPutchar(IR_TX_UART_PORT, IR_LOW);
         }
+#else
+    uart_write_bytes(IR_TX_UART_PORT, caIrLow, sizeof(caIrLow));
+#endif
     }
 }
 
@@ -169,16 +198,17 @@ uint8_t IR_ReceivePacket(uint8_t *data)
 
 void app_main(void)
 {
-    uartInit(0, 115200, 8, 0, 1, UART_TX_PIN, UART_RX_PIN);
-    uartInit(1, UART1_BAUDS, 7, 0, 1, UART1_TX, UART1_RX);
-    uartInit(2, UART2_BAUDS, 8, 0, 1, UART2_TX, UART2_RX);
+    uartInit(PC_UART_PORT, PC_UARTS_BAUD_RATE, 8, 0, 1, PC_UART_TX_PIN, PC_UART_RX_PIN);
+#if SCHEME == UART_SEND_SCHEME
+    uartInit(IR_TX_UART_PORT, IR_TX_BAUDS, IR_BIT_SIZE, 0, 1, IR_TX_TX_PIN, IR_TX_RX_PIN);
+#endif
+    uartInit(IR_RX_UART_PORT, IR_RX_BAUDS, 8, 0, 1, IR_RX_TX_PIN, IR_RX_RX_PIN);
     
-
     delayMs(500);
-    uartGoto11(UART_NUM);
+    uartGoto11(PC_UART_PORT);
     delayMs(500);
-    uartPutchar(UART_NUM, '#');
-    uartClrScr(UART_NUM);
+    uartPutchar(PC_UART_PORT, '#');
+    uartClrScr(PC_UART_PORT);
 
     // Wait for input
     delayMs(500);
@@ -186,12 +216,19 @@ void app_main(void)
     // echo forever
     while(1)
     {
-        uartPutchar(UART_NUM,uartGetchar(UART_NUM));
-        IR_SendByte('U');
+        uartPutchar(PC_UART_PORT,uartGetchar(PC_UART_PORT));
+        IR_SendByte('H');
+        IR_SendByte('e');
+        IR_SendByte('l');
+        IR_SendByte('l');
+        IR_SendByte('o');
+        IR_SendByte('!');
         delayMs(50);
-        if (uartKbhit(UART_IR_RX))
+        while (uartKbhit(IR_RX_UART_PORT))
         {
-            uartPutchar(UART_NUM,uartGetchar(UART_IR_RX));
+            uartPutchar(PC_UART_PORT,'{');
+            uartPutchar(PC_UART_PORT,uartGetchar(IR_RX_UART_PORT));
+            uartPutchar(PC_UART_PORT,'}');
         }
         
     }
